9891ce964070fb201a81ca29a25010a0b2357c7b
ZaurO5 <165468417+ZaurO5@users.noreply.github.com>
2024-07-17 00:17:18 +0400

    SQLQuery78.sql

--- SQLQuery78.sql
+++ SQLQuery78.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE Sellers (
     Id INT PRIMARY KEY,
     Name VARCHAR(50),
@@ -23,35 +24,79 @@ CREATE TABLE Orders (
     FOREIGN KEY (CustomerId) REFERENCES Customers(Id)
 );
 
-SELECT Customers.Id, Customers.Name, Customers.Surname, SUM(Orders.Amount) AS TotalAmount
-FROM Customers
-JOIN Orders ON Customers.Id = Orders.CustomerId
-GROUP BY Customers.Id, Customers.Name, Customers.Surname
-HAVING SUM(Orders.Amount) > 1000;
+SELECT 
+    Customers.Id, 
+    Customers.Name, 
+    Customers.Surname, 
+    SUM(Orders.Amount) AS TotalAmount
+FROM 
+    Customers
+JOIN 
+    Orders ON Customers.Id = Orders.CustomerId
+GROUP BY 
+    Customers.Id, Customers.Name, Customers.Surname
+HAVING 
+    SUM(Orders.Amount) > 1000;
 
-SELECT DISTINCT Customers.Name AS CustomerName, Customers.Surname AS CustomerSurname, Sellers.Name AS SellerName, Sellers.Surname AS SellerSurname, Customers.City
-FROM Customers
-JOIN Sellers ON Customers.City = Sellers.City;
+SELECT DISTINCT 
+    Customers.Name AS CustomerName, 
+    Customers.Surname AS CustomerSurname, 
+    Sellers.Name AS SellerName, 
+    Sellers.Surname AS SellerSurname, 
+    Customers.City
+FROM 
+    Customers
+JOIN 
+    Sellers ON Customers.City = Sellers.City;
 
-SELECT * FROM Orders
-WHERE OrderDate >= '2024-01-04' AND State = 'Tamamlanib';
+SELECT 
+    * 
+FROM 
+    Orders
+WHERE 
+    OrderDate >= '2024-02-05' AND State = 'Tamamdir';
 
-SELECT Sellers.Id, Sellers.Name, Sellers.Surname, COUNT(Orders.Id) AS CompletedOrders
-FROM Sellers
-JOIN Orders ON Sellers.Id = Orders.SellerId
-WHERE Orders.State = 'Tamamlanib'
-GROUP BY Sellers.Id, Sellers.Name, Sellers.Surname
-HAVING COUNT(Orders.Id) > 10;
+SELECT 
+    Sellers.Id, 
+    Sellers.Name, 
+    Sellers.Surname, 
+    COUNT(Orders.Id) AS CompletedOrders
+FROM 
+    Sellers
+JOIN 
+    Orders ON Sellers.Id = Orders.SellerId
+WHERE 
+    Orders.State = 'Tamamdir'
+GROUP BY 
+    Sellers.Id, Sellers.Name, Sellers.Surname
+HAVING 
+    COUNT(Orders.Id) > 10;
 
-SELECT Customers.Id, Customers.Name, Customers.Surname, COUNT(Orders.Id) AS OrderCount
-FROM Customers
-JOIN Orders ON Customers.Id = Orders.CustomerId
-GROUP BY Customers.Id, Customers.Name, Customers.Surname
-ORDER BY OrderCount DESC;
+SELECT 
+    Customers.Id, 
+    Customers.Name, 
+    Customers.Surname, 
+    COUNT(Orders.Id) AS OrderCount
+FROM 
+    Customers
+JOIN 
+    Orders ON Customers.Id = Orders.CustomerId
+GROUP BY 
+    Customers.Id, Customers.Name, Customers.Surname
+ORDER BY 
+    OrderCount DESC;
 
-SELECT Orders.OrderDate, Orders.Id, Sellers.Name, Sellers.Surname, Sellers.City
-FROM Orders
-JOIN Sellers ON Orders.SellerId = Sellers.Id
-WHERE Orders.State != 'Tamamlanib'
-ORDER BY Orders.OrderDate ASC;
-
+SELECT 
+    Orders.OrderDate, 
+    Orders.Id, 
+    Sellers.Name, 
+    Sellers.Surname, 
+    Sellers.City
+FROM 
+    Orders
+JOIN 
+    Sellers ON Orders.SellerId = Sellers.Id
+WHERE 
+    Orders.State != 'Tamamdir'
+ORDER BY 
+    Orders.OrderDate ASC;
